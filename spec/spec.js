b118c7adb601dfa4f3f3d4946c4ea2c299eff94f
Nikhil Marathe <nsm.nikhil@gmail.com>
2011-11-16 15:01:31 +0530

    Added usage function and basic usage parser.
    The parser recognizes short and long options and their
    descriptions, but no groups or default values yet.

--- spec/spec.js
+++ spec/spec.js
@@ -8,9 +8,14 @@ var onlySynopsis = 'bup save [-tc] [-n name] <filenames...>';
 vows.describe('rational')
 .addBatch({
     'Usage builder with no options': {
+        topic: new Rational('rational'),
+        'usage is a function': function(rat) {
+            assert.isFunction(rat.usage);
+        },
+
         '_usageStr should give usage': {
             topic: new Rational('rational'),
-            'usage should match': function (rat) {
+            'usage should match': function(rat) {
                 assert.equal(rat._usageStr, 'Usage: rational');
             }
         },
@@ -25,8 +30,25 @@ vows.describe('rational')
         'stop processing on --': {
             topic: new Rational('rational\n--\nrational <filenames...>'),
             'usage should only be rational': function(rat) {
-                assert.equal(rat._usageStr, 'Usage: rational');
+                assert.isNotNull(rat._usageStr.match('Usage: rational\n\nOptions\n'));
             }
+        }
+    },
+
+    'Usage builder with options (no groups)': {
+        topic: new Rational('wget [OPTION...] [URL...]\n--\n'+
+                            'V,version display the version of Wget and exit\n'+
+                            'h,help print this help\n'+
+                            'b,background go to background after startup\n'+
+                            'retry-connrefused retry even if connection is refused'),
+        '_usageStr should match': function(rat) {
+            rat.usage();
+            var lines = rat._usageStr.split('\n');
+            assert.equal(lines[0], 'Usage: wget [OPTION...] [URL...]');
+            assert.isNotNull(lines[3].match('  -V, --version       \tdisplay the version of Wget and exit'));
+            assert.isNotNull(lines[4].match('  -h, --help          \tprint this help'));
+            assert.isNotNull(lines[5].match('  -b, --background    \tgo to background after startup'));
+            assert.isNotNull(lines[6].match('  --retry-connrefused \tretry even if connection is refused'));
         }
     },
 
